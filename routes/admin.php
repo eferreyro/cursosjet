68a7f6145c9f71d7c3718d2b477b4d7a32a60601
eferreyro <emafe7472@gmail.com>
2021-03-26 19:07:05 -0600

    Agregando roles y permisos con Spatie

--- routes/admin.php
+++ routes/admin.php
@@ -3,5 +3,10 @@
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\Admin\HomeController;
+use App\Http\Controllers\Admin\RoleController;
 
-Route::get('', [HomeController::class, 'index']);
+Route::get('', [HomeController::class, 'index'])->name('home');
+
+//Genero ruta de tipo resource para los Roles (7 rutas necesarias para 1 CRUD)
+
+Route::resource('roles', RoleController::class)->names('roles');
