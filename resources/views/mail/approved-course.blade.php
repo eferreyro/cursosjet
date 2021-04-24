78f3beff8cc6280e90a2ec3d763a4b14122ee90c
eferreyro <emafe7472@gmail.com>
2021-04-24 17:42:08 -0500

    Mejorando la vista de mail

--- resources/views/mail/approved-course.blade.php
+++ resources/views/mail/approved-course.blade.php
@@ -5,14 +5,24 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
-</head>
+
 <style>
-    h1{
-        color:cadetblue;
+    .bgImage{
+        background: rgb(238,174,202);
+        background: radial-gradient(circle, rgba(238,174,202,1) 0%, rgba(148,187,233,1) 100%);
+        background-repeat: no-repeat;
+        background-size: auto;
     }
+    h1{
+        color:#9932cc;
+    }
+
 </style>
+</head>
 <body>
-    <h1>Este es un email de prueba</h1>
+    <div class="bgImage">
+    <h1 class="colorH1">Este es un email de prueba</h1>
     <p>El curso <strong>{{$course->title}}</strong> se ha publicado con exito</p>
+    </div>
 </body>
 </html>
